b567af0d6f8360633f18d4c9c41d3fd12492aa55
Matt Aspen <mattaspen@gmail.com>
2018-05-27 22:57:02 -0700

    got search to switch over from add and vice versa, got add  movies to persist

--- src/components/App.js
+++ src/components/App.js
@@ -12,48 +12,107 @@ class App extends React.Component {
 			],
 			movies: [],
 			searchQuery: '',
-      newMovie: ''
+      addMovies: [
+        {title: 'First Added Movie'}
+      ],
+      addMovie: '',
+      searchBoxText: true,
+      addBoxText: false
+
     };
 
 		this.state.movies = this.state.stubData;
+
+    // bind actions/buttons to 'this'
+    this.addMovieButtonClick = this.addMovieButtonClick.bind(this);
+    this.handleAddMoveInputChange = this.handleAddMoveInputChange.bind(this);
+    this.searchMoviesButtonClick = this.searchMoviesButtonClick.bind(this);
+    this.handleSearchBoxInputChange = this.handleSearchBoxInputChange.bind(this);
+
+    this.restoreSearchMovies = this.restoreSearchMovies.bind(this);
+    this.restoreAddMovies = this.restoreAddMovies.bind(this);
+  }
+
+  componentWillMount() {
+    // ccalled right after constructor
   }
 
   componentDidMount() {
-  	//
+  	// called afte render()
   };
 
+  /** ---------- Search --------- */
+
   handleSearchBoxInputChange(text) {
+    if (this.state.searchBoxText) {
+      this.state.searchBoxText = false;
+      this.state.addBoxText = true;
+      this.restoreSearchMovies();
+    }
   	this.setState({ searchQuery: text });	
   };
  
 	searchMoviesButtonClick() {
-		this.searchMovies();
+    event.preventDefault();
+    this.searchMovies();
 	};
 
 	searchMovies() {
 		var movieMatchesArray = this.state.movies.filter
 			(movie => movie.title.includes(this.state.searchQuery)); // want partials as well
+
 		if(movieMatchesArray.length > 0) {
-			this.setState({ movies: movieMatchesArray});
+			this.setState({ movies: movieMatchesArray} );
+
+      console.log('this.state.movies AFTER search results', this.state.movies);
 		} else {
-			this.state.movies = [];
-			this.setState({ movies: [{title: '\"' + this.state.searchQuery + '\" was not found'}] });		
+      this.state.movies = [];
+			this.setState({ movies: [{title: '\"' + this.state.searchQuery + '\" was not found'}] });
+
+      console.log('this.state.movies AFTER no movie found', this.state.movies);
 		}
 	}
-  
-  handleAddMoveInputChange(text) {
-// console.log('handleAddMoveInputChange txt ', text);
-    this.setState({ newMovie: text });    
+
+  restoreSearchMovies() {
+    this.setState({ movies: this.state.stubData });
   }
   
+  /** ---------- Add --------- */
+
+  handleAddMoveInputChange(text) {
+    if(this.state.addBoxText) {
+      this.state.addBoxText = false;
+      this.state.searchBoxText = true;
+      this.restoreAddMovies();
+    }
+    this.setState({ addMovie: text });
+  }
+
   addMovieButtonClick() {
-console.log('addMovieButtonClick');
-    this.addMovie;
+    this.addMovie();
   }
   
   addMovie() {
-console.log('addMovie');     
+    if(this.state.addMovie !== null) {
+
+      console.log('movie to add:', this.state.addMovie);
+      // TODO: check if 'movies' already contains this title
+      //this.setState({ movies: [...this.state.addMovies, {title: this.state.addMovie}] });
+
+      this.state.addMovies = [...this.state.addMovies, {title: this.state.addMovie}];
+      this.setState({ movies: this.state.addMovies });
+
+      console.log('this.state.movies AFTER movie added', this.state.movies);
+    }
   }
+
+  restoreAddMovies() {
+    this.setState({ movies: this.state.addMovies });
+  }
+
+  // doesAddMovieExist(addMovie) {
+  //   this.state.movies
+  // }
   
 	render() {
     return (
@@ -66,14 +125,14 @@ console.log('addMovie');
         <div className="row">
           <div className="col-md-4" />
           <div className="col-md-4">
-            <AddMovieBar add={this.addMovieButtonClick.bind(this)} textBoxChange={this.handleAddMoveInputChange.bind(this)}/>
+            <AddMovieBar add={this.addMovieButtonClick} textBoxChange={this.handleAddMoveInputChange}/>
           </div>
           <div className="col-md-4" />
         </div>
         <div className="row">
           <div className="col-md-4" />
           <div className="col-md-4">
-            <SearchBar search={this.searchMoviesButtonClick.bind(this)} textBoxChange={this.handleSearchBoxInputChange.bind(this)} />
+            <SearchBar search={this.searchMoviesButtonClick} textBoxChange={this.handleSearchBoxInputChange} />
           </div>
           <div className="col-md-4" />
         </div>
